7271d590843129f72d12f15dc757d4a320f50277
Dev-gs-Yoon <seokseok9773@gmail.com>
2025-07-24 16:45:58 +0900

    about object poly

--- lang/object/poly/ObjectPolyExample1.java
+++ lang/object/poly/ObjectPolyExample1.java
@@ -1,6 +1,7 @@
 package lang.object.poly;
 
 public class ObjectPolyExample1 {
+    /** main 함수입니다*/
     public static void main(String[] args) {
         Dog dog = new Dog();
         Car car = new Car();
